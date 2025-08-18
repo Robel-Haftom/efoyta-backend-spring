c6193f610d097824355365e7a448d23b9efc38db
Robel Haftom <robelhaftom80@gmail.com>
2025-08-18 19:20:45 +0300

    getting user successfully

--- src/main/java/com/wegagen/efoyta/service/Impl/AuthServiceImpl.java
+++ src/main/java/com/wegagen/efoyta/service/Impl/AuthServiceImpl.java
@@ -2,11 +2,8 @@ package com.wegagen.efoyta.service.Impl;
 
 import com.wegagen.efoyta.dto.request.LoginRequestDto;
 import com.wegagen.efoyta.dto.response.LoginResponseDto;
-import com.wegagen.efoyta.entity.User;
 import com.wegagen.efoyta.exception.ResourceNotFoundException;
-import com.wegagen.efoyta.mapper.UserMapper;
 import com.wegagen.efoyta.repository.MssqlViewRepository;
-import com.wegagen.efoyta.repository.UserRepository;
 import com.wegagen.efoyta.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,15 +12,25 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
-    private final UserRepository userRepository;
     private final MssqlViewRepository mssqlViewRepository;
 
 
     @Override
     public LoginResponseDto login(LoginRequestDto requestDto) {
-        User user = userRepository.findActiveUser(requestDto.getUserName())
+        return mssqlViewRepository.findActiveUserByUsername(requestDto.getUserName())
+                .map(row -> new LoginResponseDto(
+                        (String) row.get("Fullname"),
+                        (String) row.get("EmployeeName"),
+                        row.get("Account_type") == null ? null : row.get("Account_type").toString(),
+                        row.get("BRANCH_CODE") == null ? null : row.get("BRANCH_CODE").toString(),
+                        (String) row.get("workunit"),
+                        row.get("Cluster_id") == null ? null : row.get("Cluster_id").toString(),
+                        row.get("position_id") == null ? null : row.get("position_id").toString(),
+                        row.get("workstation_id") == null ? null : row.get("workstation_id").toString(),
+                        (String) row.get("position"),
+                        (String) row.get("Cluster_Name")
+                ))
                 .orElseThrow(() -> new ResourceNotFoundException("No user found with this user name: " + requestDto.getUserName()));
-        return UserMapper.mapToLoginResponseDto(user);
     }
 
     @Override
